6d7cb7b8f22a698e635b5d41269ac102a08eac3a
Diwakar Jha <diwakarjha554@gmail.com>
2024-05-27 16:33:58 +0530

    about updated

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,8 +10,16 @@ const About = () => {
       <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
         <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">About</h2>
       </div>
-      <div>
-        <p className="mb-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem consectetur temporibus quos ad quam ex vero, et maxime eligendi explicabo qui corporis ducimus. Quasi non repudiandae, voluptatem dolor dolorem praesentium ullam magni sit id vel architecto deserunt quas, fugit, officia ipsam dolorum hic alias enim cumque cupiditate mollitia est excepturi labore. Delectus voluptatem fugiat tenetur est quod pariatur quis quae libero tempore, harum eos ut, aliquam autem cum officiis facere. Aliquam voluptate quidem consequuntur adipisci illum unde laborum repudiandae voluptatem magni quis suscipit cum nihil labore tempore praesentium hic deleniti et vel recusandae, nostrum eius quia. Omnis quod quae quaerat!</p>
+      <div className="flex flex-col gap-7">
+        <p>
+          I craft user-friendly travel experiences at <span className="text-white font-[500]">Travel Trail Holidays</span>.  Leveraging Next.js, MongoDB, Node.js, and Express.js, I build accessible and sustainable travel booking platforms. My prior experience with Django, Python, and JavaScript ensures I can adapt across technologies.
+        </p>
+        <p>
+          But code is just a tool. I'm driven by a passion to create positive change.  I actively seek opportunities to contribute meaningful features within the travel industry, like accessibility options or highlighting eco-friendly travel choices. My goal is to make travel not just seamless, but also inclusive and sustainable.
+        </p>
+        <p>
+          This dedication extends beyond Travel Trail Holidays. I contribute to open-source projects and explore freelance work for social impact organizations. My ultimate goal is to use technology as a bridge to a better future, and I'm constantly seeking ways to leverage my expertise for the greater good.
+        </p>
       </div>
     </Section>
   )
